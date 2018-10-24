8e72169c1f2739ad95ba640ac1d2992384f0f29a
liusihua <1398164936@qq.com>
2018-10-24 21:05:01 +0800

    添加测试类

---
+++ src/main/java/com/mlink/base/entity/Material.java
@@ -0,0 +1,118 @@
+package com.mlink.base.entity;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+/**
+ * 实体类对应的数据表为：  material
+ * @author mlink
+ * @date 2018-10-24 21:04:37
+ */
+@ApiModel(value ="测试类")
+public class Material {
+    @ApiModelProperty(value = "主键")
+    private String materialId;
+
+    @ApiModelProperty(value = "原料名称")
+    private String materialName;
+
+    @ApiModelProperty(value = "原料数量")
+    private Integer materialNum;
+
+    @ApiModelProperty(value = "")
+    private String materialDescription;
+
+    @ApiModelProperty(value = "创建时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    @ApiModelProperty(value = "创建人")
+    private String createUser;
+
+    @ApiModelProperty(value = "修改时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "修改人")
+    private String updateUser;
+
+    @ApiModelProperty(value = "删除标志	0：未删除，1：已删除")
+    private String deleteFlag;
+
+    public String getMaterialId() {
+        return materialId;
+    }
+
+    public void setMaterialId(String materialId) {
+        this.materialId = materialId;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public Integer getMaterialNum() {
+        return materialNum;
+    }
+
+    public void setMaterialNum(Integer materialNum) {
+        this.materialNum = materialNum;
+    }
+
+    public String getMaterialDescription() {
+        return materialDescription;
+    }
+
+    public void setMaterialDescription(String materialDescription) {
+        this.materialDescription = materialDescription;
+    }
+
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    @JsonFormat(locale = "zh", timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public String getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(String deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+}
